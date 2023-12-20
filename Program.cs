25ee3f719fe5b70ae38dc66bd198e123e86610dd
Andrew <withapoll@outlook.com>
2023-12-20 11:31:44 +0100

    connected to DoubleB

--- Program.cs
+++ Program.cs
@@ -5,9 +5,24 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddDbContext<DoubleBContext>(options =>
     options.UseNpgsql(builder.Configuration.GetConnectionString("localdb")));
+
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("VueCorsPolicy", builder =>
+    {
+        builder
+        .AllowAnyHeader()
+        .AllowAnyMethod()
+        .AllowCredentials()
+        .WithOrigins("http://localhost:8080");
+    });
+});
+
 var app = builder.Build();
 
 app.UseHttpsRedirection();
 app.MapControllers();
+
+app.UseCors("VueCorsPolicy");
 
 app.Run();
